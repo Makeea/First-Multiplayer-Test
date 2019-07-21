39deab519ed76f9cfb78bd4990b19ba28b560ba6
Makeea <speedynyc@gmail.com>
2019-07-21 17:59:03 -0400

    Update networkManager.cs
    Connecting to room.
    showing that the room is connected.

--- Assets/Scripts/networkManager.cs
+++ Assets/Scripts/networkManager.cs
@@ -27,11 +27,24 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         PhotonNetwork.ConnectUsingSettings();
     }
 
+   
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master Server");
+        CreateRoom("testroom");
+
+    }
+
+    //Attempts to Create a room
     public void CreateRoom (string roomName)
     {
         PhotonNetwork.CreateRoom(roomName);
     }
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created Room:  " + PhotonNetwork.CurrentRoom.Name);
 
+    }
     public void JoinRoom(string roomName)
     {
         PhotonNetwork.JoinRoom(roomName);
