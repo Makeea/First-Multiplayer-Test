d4bd0bf02572826a67a2cf0f28dd3db73d784171
Makeea <speedynyc@gmail.com>
2019-07-25 01:21:42 -0400

    Building player
    unity stuff for player
    script feilds for playcontrollor

---
+++ Assets/Scripts/PlayerControllor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerControllor : MonoBehaviour
+{
+    [HideInInspector]
+    public int id;
+
+    [Header("Info")]
+    public float moveSpeed;
+    public float jumpSpeed;
+    public GameObject hatObject;
+
+    [HideInInspector]
+    public float curHatTime;
+
+    [Header("Components")]
+    public Rigidbody rig;
+    public Player photonPlayer;
+
+
+}
