b915c9d90148759075928b6df24fc99166c79186
Makeea <speedynyc@gmail.com>
2019-07-25 01:51:33 -0400

    setting up game manager for spawns

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [Header("Stats")]
+    public bool gameEnded = false;
+    public float timeToWin;
+    public float invincibleDuration;
+    private float hatPickupTime;
+
+    [Header("Players")]
+    public string playerPrefabLocation;
+    public Transform[] spawnPoints;
+    public PlayerControllor[] players;
+    public int playerWithHat;
+    private int playersInGame;
+
+    //instance
+    public static GameManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        players = new PlayerControllor[PhotonNetwork.PlayerList.Length];
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+
+        // is all players in the scene?
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            SpawnPlayer();
+    }
+
+    // spawns a player and initilizes them
+    void SpawnPlayer()
+    {
+        // Instantiates the player across the network
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+
+        // add PlayerControllor script to each player
+        PlayerControllor playerScript = playerObj.GetComponent<PlayerControllor>();
+    }
+}
